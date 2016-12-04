e587ee2113ac48f09a79611b00918a66189f72e1
Jozsef Hegedus <jhegedus42@gmail.com>
2016-12-04 17:22:07 +0200

    end of lecture II/18 - applying redux middlewares

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,45 +1,15 @@
 // @flow
 
 import throttle from 'lodash/throttle';
-import { createStore , combineReducers} from 'redux'
+import { createStore , combineReducers, applyMiddleware} from 'redux'
 import type {S_Todo,State$TodoMap,State$App,StoreType } from './types/state_types.js';
 import todoApp  from './reducers/reducers.js'
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 
-//type hof = <I1, O1, I2, O2> (fn: (t1: I1) => O1) => ((t1: I2) => O2);
-
-//type ty_MiddleWare = (store:StoreType) => hof;
-
-const logger = (store) => (next)=>(action)=>{
-  console.group(action.type);
-  console.log('%c prev state','color: gray', store.getState());
-  console.log('%c action','color: blue',action);
-  const returnValue=next(action);
-  console.log('%c next state','color: green',store.getState());
-  console.groupEnd();
-  return returnValue;
-
-};
-
-
-const promise = (store) => (next)=> (action) :any => {
-  if (typeof action.then === 'function') {
-    return action.then(next);
-  };
-  return next(action);
-};
-
-const wrapDispatchWithMiddleWares = (store:StoreType, middlewares)=>{
-  middlewares.slice().reverse().forEach(middleware=>{
-        const next=store.dispatch;
-        store.dispatch=middleware(store)(next);
-      }
-    );
-};
 
 const configureStore = () =>{
-  const store:StoreType =  createStore (todoApp);
-  const middlewares=[promise,logger];
-  wrapDispatchWithMiddleWares(store, middlewares);
-  return store;
+  const middlewares=[promise,createLogger()];
+  return createStore (todoApp,applyMiddleware(...middlewares));
 };
 export default configureStore;
