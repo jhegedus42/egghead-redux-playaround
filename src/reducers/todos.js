82c8c0846f2f35b8a87c09dec1b200f825044c73
Jozsef Hegedus <jhegedus42@gmail.com>
2016-12-05 17:26:40 +0200

    end of lecture II/20 refactoring

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,10 +1,9 @@
 // @flow
 import type { State$TodoIDsByFilterMap, State$Filter, State$TodosByIDMap,State$Root}  from '../types/state_types'
-import {S_Todo,TodoID, getAllTodos} from '../types/state_types'
-import {mk_A_ADD_TODO,mk_A_TOGGLE_TODO,Actions} from '../types/action_types'
+import {S_Todo,TodoID} from '../types/state_types'
 import type {A_TODO} from '../types/action_types'
-import {combineReducers} from 'redux'
 import byIdReducer, * as fromById from './byIdReducer';
+import createList, * as fromList from './createList';
 
 const initState={
                         byId:{
@@ -19,53 +18,16 @@ const initState={
 
 
 
-const allIdsReducer=(state:TodoID[]=[],action:A_TODO):TodoID[]=>{
-  switch (action.type){
-    case Actions.RECIEVE_TODOS:
-      if (action.filter !== 'all'){
-        return state;
-      }
-      return action.serverResponse.map(todo=> todo.todoId);
-    default:
-      return state;
-  }
-}
-
-const activeIdsReducer=(state:TodoID[]=[],action:A_TODO):TodoID[]=>{
-  switch (action.type){
-    case Actions.RECIEVE_TODOS:
-      if (action.filter !== 'active'){
-        return state;
-      }
-      return action.serverResponse.map(todo=> todo.todoId);
-    default:
-      return state;
-  }
-}
-
-const completedIdsReducer=(state:TodoID[]=[],action:A_TODO):TodoID[]=>{
-  switch (action.type){
-    case Actions.RECIEVE_TODOS:
-      if (action.filter !== 'completed'){
-        return state;
-      }
-      return action.serverResponse.map(todo=> todo.todoId);
-    default:
-      return state;
-  }
-}
-
-
 const idsByFilterReducer = (state:State$TodoIDsByFilterMap, action:A_TODO) : State$TodoIDsByFilterMap => ({
-  all:allIdsReducer(state['all'], action),
-  active:activeIdsReducer(state['active'], action),
-  completed:completedIdsReducer(state['completed'],action)
+  all:createList('all')(state['all'], action),
+  active:createList('active')(state['active'], action),
+  completed:createList('completed')(state['completed'],action)
 });
 
 
 export const getVisibleTodos  = (state:State$Root, filter:State$Filter) : S_Todo[] => { // selector
-  const ids= state.idsByFilter[filter];
-  return ids.map(todoId=> state.byId[todoId.id]);
+  const ids= fromList.getIds(state.idsByFilter[filter]);
+  return ids.map(todoId=> fromById.getTodo(state.byId,todoId.id));
 };
 
 
